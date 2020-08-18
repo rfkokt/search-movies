cbda1e7aa1a7c18d743ce62e4d6faecc1716af07
Nadilla Rahma <rfkokt105@MacBook-Air-Rfkokt105.local>
2020-08-19 00:35:13 +0700

    Updates

---
+++ precache-manifest.d11ee6b2f4201e4b57e33cfdabf2452e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b52bc648dd02f9c06e3347bd34b97f1d",
+    "url": "/search-movies/index.html"
+  },
+  {
+    "revision": "d6d593b67af11ddcfa34",
+    "url": "/search-movies/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "c452840277e00c556c76",
+    "url": "/search-movies/static/css/main.e66dd926.chunk.css"
+  },
+  {
+    "revision": "d6d593b67af11ddcfa34",
+    "url": "/search-movies/static/js/2.10cccdd5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/search-movies/static/js/2.10cccdd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c452840277e00c556c76",
+    "url": "/search-movies/static/js/main.e0c6a2f8.chunk.js"
+  },
+  {
+    "revision": "dc39b4fc61d372fb2539",
+    "url": "/search-movies/static/js/runtime-main.6d069703.js"
+  },
+  {
+    "revision": "42f999413677f4d23d05d4a84fd5efeb",
+    "url": "/search-movies/static/media/felix-mooneeram-evlkOfkQ5rE-unsplash.42f99941.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/search-movies/precache-manifest.d11ee6b2f4201e4b57e33cfdabf2452e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/search-movies/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
